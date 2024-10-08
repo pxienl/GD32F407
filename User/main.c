643461296e5af5f519cafa890c4ffd5f4d244c2e
pxienl <pxienl@outlook.com>
2024-10-08 19:04:08 +0800

    test: add NTC test

--- User/main.c
+++ User/main.c
@@ -2,146 +2,221 @@
 #include "systick.h"
 #include "DMA.h"
 #include "USART.h"
+#include "ADC.h"
 #include "GPIO.h"
-#include "u8g2/u8g2.h"
-#include "I2C.h"
 
-// #define I2C_SOFTWARE
-
-void RX0_recv(uint8_t *rxbuffer,uint32_t len){
-    usart_dma_send(USART0,rxbuffer,len);
-}
-
-
-uint8_t u8g2_gpio_and_delay_gd32_software(U8X8_UNUSED u8x8_t *u8x8, U8X8_UNUSED uint8_t msg, U8X8_UNUSED uint8_t arg_int, U8X8_UNUSED void *arg_ptr)
-{
-	switch(msg){
-		//Initialize SPI peripheral
-		case U8X8_MSG_GPIO_AND_DELAY_INIT:
-			/* HAL initialization contains all what we need so we can skip this part. */
-
-		break;
-
-		//Function which implements a delay, arg_int contains the amount of ms
-		case U8X8_MSG_DELAY_MILLI:
-		    delay_1ms(arg_int);
-
-		break;
-		//Function which delays 10us
-		case U8X8_MSG_DELAY_10MICRO:
-		    delay_1us(10);
-
-		break;
-		//Function which delays 100ns
-		case U8X8_MSG_DELAY_100NANO:
-		    __NOP();
-
-		break;
-
-    #ifdef I2C_SOFTWARE
-        case U8X8_MSG_GPIO_I2C_CLOCK:
-			if (arg_int) gpio_bit_set(GPIOB,GPIO_PIN_6);
-			else gpio_bit_reset(GPIOB,GPIO_PIN_6);
-        break;
-
-        case U8X8_MSG_GPIO_I2C_DATA:
-            if (arg_int) gpio_bit_set(GPIOB,GPIO_PIN_7);
-			else gpio_bit_reset(GPIOB,GPIO_PIN_7);
-		break;
-    #endif
-
-		default:
-			return 0; //A message was received which is not implemented, return 0 to indicate an error
-	}
-
-	return 1; // command processed successfully.
-}
-
-uint8_t u8g2_byte_cb_hardware(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr)
-{
-	switch(msg){
-        case U8X8_MSG_BYTE_SEND:
-          for (uint32_t i = 0; i < arg_int; i++)
-          {
-            I2C_hardware_send(I2C0,*((uint8_t *)arg_ptr + i));
-          }
-          
-          break;
-        case U8X8_MSG_BYTE_INIT:
-          break;
-        case U8X8_MSG_BYTE_SET_DC:
-          break;
-        case U8X8_MSG_BYTE_START_TRANSFER:
-          if ( u8x8->display_info->i2c_bus_clock_100kHz >= 4 )
-          {
-    	      i2c_clock_config(I2C0,400000,I2C_DTCY_2);
-          }
-
-          I2C_hardware_start(I2C0,1);
-          I2C_hardware_address(I2C0,u8x8_GetI2CAddress(u8x8),I2C_TRANSMITTER);
-          break;
-        case U8X8_MSG_BYTE_END_TRANSFER:
-          I2C_hardware_stop(I2C0);
-          break;
-
-		default:
-			return 0; //A message was received which is not implemented, return 0 to indicate an error
-	}
-
-	return 1; // command processed successfully.
-}
+uint16_t temp_table[]= {
+	58354, // -55
+	55464, // -54
+	52698, // -53
+	50048, // -52
+	47515, // -51
+	45097, // -50
+	42789, // -49
+	40589, // -48
+	38492, // -47
+	36496, // -46
+	34597, // -45
+	32791, // -44
+	31075, // -43
+	29444, // -42
+	27896, // -41
+	26427, // -40
+	25034, // -39
+	23713, // -38
+	22460, // -37
+	21273, // -36
+	20148, // -35
+	19083, // -34
+	18075, // -33
+	17120, // -32
+	16216, // -31
+	15361, // -30
+	14551, // -29
+	13785, // -28
+	13061, // -27
+	12376, // -26
+	11728, // -25
+	11114, // -24
+	10535, // -23
+	9986,  // -22
+	9468,  // -21
+	8977,  // -20
+	8513,  // -19
+	8075,  // -18
+	7660,  // -17
+	7267,  // -16
+	6896,  // -15
+	6545,  // -14
+	6212,  // -13
+	5898,  // -12
+	5601,  // -11
+	5319,  // -10
+	5053,  // -9
+	4801,  // -8
+	4562,  // -7
+	4336,  // -6
+	4122,  // -5
+	3920,  // -4
+	3728,  // -3
+	3546,  // -2
+	3374,  // -1
+	3211,  // 0
+	3057,  // 1
+	2910,  // 2
+	2771,  // 3
+	2639,  // 4
+	2515,  // 5
+	2396,  // 6
+	2284,  // 7
+	2177,  // 8
+	2076,  // 9
+	1978,  // 10
+	1889,  // 11
+	1802,  // 12
+	1720,  // 13
+	1642,  // 14
+	1568,  // 15
+	1497,  // 16
+	1430,  // 17
+	1366,  // 18
+	1306,  // 19
+	1248,  // 20
+	1193,  // 21
+	1141,  // 22
+	1092,  // 23
+	1044,  // 24
+	1000,  // 25
+	957,   // 26
+	916,   // 27
+	877,   // 28
+	840,   // 29
+	805,   // 30
+	771,   // 31
+	739,   // 32
+	709,   // 33
+	679,   // 34
+	652,   // 35
+	625,   // 36
+	600,   // 37
+	576,   // 38
+	552,   // 39
+	530,   // 40
+	509,   // 41
+	489,   // 42
+	470,   // 43
+	452,   // 44
+	434,   // 45
+	417,   // 46
+	401,   // 47
+	386,   // 48
+	371,   // 49
+	358,   // 50
+	344,   // 51
+	331,   // 52
+	318,   // 53
+	306,   // 54
+	295,   // 55
+	284,   // 56
+	274,   // 57
+	264,   // 58
+	254,   // 59
+	245,   // 60
+	236,   // 61
+	228,   // 62
+	220,   // 63
+	212,   // 64
+	205,   // 65
+	198,   // 66
+	191,   // 67
+	184,   // 68
+	178,   // 69
+	172,   // 70
+	166,   // 71
+	160,   // 72
+	155,   // 73
+	150,   // 74
+	145,   // 75
+	140,   // 76
+	135,   // 77
+	131,   // 78
+	126,   // 79
+	122,   // 80
+	118,   // 81
+	115,   // 82
+	111,   // 83
+	107,   // 84
+	104,   // 85
+	101,   // 86
+	97,    // 87
+	94,    // 88
+	91,    // 89
+	89,    // 90
+	86,    // 91
+	83,    // 92
+	81,    // 93
+	78,    // 94
+	76,    // 95
+	74,    // 96
+	71,    // 97
+	69,    // 98
+	67,    // 99
+	65,    // 100
+	63,    // 101
+	61,    // 102
+	60,    // 103
+	58,    // 104
+	56,    // 105
+	55,    // 106
+	53,    // 107
+	52,    // 108
+	50,    // 109
+	49,    // 110
+	47,    // 111
+	46,    // 112
+	45,    // 113
+	43,    // 114
+	42,    // 115
+	41,    // 116
+	40,    // 117
+	39,    // 118
+	38,    // 119
+	37,    // 120
+	36,    // 121
+	35,    // 122
+	34,    // 123
+	33,    // 124
+	32,    // 125
+};
 
 int main(void)
 {
+    float o,vntc;
+    uint16_t i,adc;
+    int c;
     nvic_priority_group_set(NVIC_PRIGROUP_PRE2_SUB2);
     systick_config();
     usart_init(USART0,GPIOA,GPIO_AF_7,GPIO_PIN_9 | GPIO_PIN_10);
     usart_dma_tx_init(USART0,DMA1,DMA_CH7);
-    usart_dma_rx_init(USART0,DMA1,DMA_CH5);
+    // usart_dma_rx_init(USART0,DMA1,DMA_CH5);
 
-    printf("init");
+    // uint16_t buffer[2] = {0};
+    // uint8_t channels[] = {ADC_CHANNEL_14,ADC_CHANNEL_16};
 
-    u8g2_t u8g2;
-
-    #ifdef I2C_SOFTWARE
-      GPIO_output_init(GPIOB,GPIO_PUPD_PULLUP,GPIO_OTYPE_OD,GPIO_PIN_6|GPIO_PIN_7);
-      u8g2_Setup_ssd1306_i2c_128x64_noname_f(&u8g2,U8G2_R0,u8x8_byte_sw_i2c,u8g2_gpio_and_delay_gd32_software);
-    #else
-      GPIO_af_init(GPIOB,GPIO_AF_4,GPIO_PUPD_PULLUP,GPIO_PIN_6|GPIO_PIN_7);
-      I2C_hardware_init(I2C0,100000);
+    GPIO_analog_init(GPIOA,GPIO_PUPD_NONE,GPIO_PIN_1);
+    ADC_single_init(ADC0,ADC_CHANNEL_1);
+    
+    // ADC_multiple_dma_init(ADC0, channels,2, buffer,DMA1,DMA_CH4,DMA_SUBPERI0);
 
 
-      /*!
-          \brief    initialize u8g2 with full frame buffer
-          \param[in]  rotation: Rotation procedure
-            \arg        U8G2_R0: No rotation, landscape
-            \arg        U8G2_R1: 90 degree clockwise rotation
-            \arg        U8G2_R2: 180 degree clockwise rotation
-            \arg        U8G2_R3: 270 degree clockwise rotation
-            \arg        U8G2_MIRROR: No rotation, landscape, display content is mirrored (v2.6.x)
-          \param[in]  byte_cb: Either one of the existing procedures or a custom procedure for the target controller.
-            \arg        u8x8_byte_4wire_sw_spi: Standard 8-bit SPI communication with "four pins" (SCK, MOSI, DC, CS)
-            \arg        u8x8_byte_3wire_sw_spi: 9-bit communication with "three pins" (SCK, MOSI, CS)
-            \arg        u8x8_byte_8bit_6800mode: Parallel interface, 6800 format
-            \arg        u8x8_byte_8bit_8080mode: Parallel interface, 8080 format
-            \arg        u8x8_byte_sw_i2c: Two wire, I2C communication
-            \arg        u8x8_byte_ks0108: Special interface for KS0108 controller
-          \param[in]  gpio_and_delay_cb:  Lowlevel delay and GPIO procedure
-
-          \retval     none
-      */
-      u8g2_Setup_ssd1306_i2c_128x64_noname_f(&u8g2,U8G2_R0,u8g2_byte_cb_hardware,u8g2_gpio_and_delay_gd32_software);
-    #endif
-
-    u8g2_InitDisplay(&u8g2); // send init sequence to the display, display is in sleep mode after this,
-    u8g2_SetPowerSave(&u8g2, 0); // wake up display
-    // 画两条线试试
-    u8g2_DrawLine(&u8g2, 0,0, 127, 63);
-    u8g2_DrawLine(&u8g2, 127,0 , 0,63);
-    u8g2_SendBuffer(&u8g2); // 将数据发送到屏幕
-
-	while(1) 
-	{
-		
-	}
+    while(1) {
+      adc = ADC_get(ADC0,ADC_INSERTED_CHANNEL_0);
+      vntc = adc * 3.3 / 4096 ;
+      o = vntc * 1000 / (3.3 - vntc);
+      for (i = 0; i < 181 && temp_table[i] > o; i++);
+      c = i - 55;
+      
+      printf("%d,%d\n", adc,c);
+      delay_1ms(100);
+    }
 }
